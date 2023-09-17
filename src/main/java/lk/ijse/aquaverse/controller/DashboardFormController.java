e4f0949c4cc28f5b6f4e8e5606eed965bc1654ea
ashan <ashannvn@gmail.com>
2023-09-17 23:49:15 +0530

    Dashboard Error fixed

--- src/main/java/lk/ijse/aquaverse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/aquaverse/controller/DashboardFormController.java
@@ -12,6 +12,13 @@ public class DashboardFormController {
 
     public JFXButton btnHome;
     public AnchorPane bodyPane;
+    public JFXButton btnEditProfile;
+    public JFXButton btnLogOut;
+    public JFXButton btnReportsManagement;
+    public JFXButton btnStockManagement;
+    public JFXButton btnAttendenceManagement;
+    public JFXButton btnEmployeeManagement;
+    public JFXButton btnProcessControl;
 
     public void btnHomeOnAction(ActionEvent actionEvent) throws IOException {
         bodyPane.getChildren().clear();
@@ -23,6 +30,31 @@ public class DashboardFormController {
         bodyPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/ProcessControlForm.fxml"))));
     }
 
-    public void btnEmployeeManagementOnAction(ActionEvent actionEvent) {
+    public void btnEmployeeManagementOnAction(ActionEvent actionEvent) throws IOException {
+        bodyPane.getChildren().clear();
+        bodyPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/EmployeeManagementForm.fxml"))));
+    }
+
+    public void btnAttendenceManagementOnAction(ActionEvent actionEvent) throws IOException {
+        bodyPane.getChildren().clear();
+        bodyPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/EmployeeAttendence.fxml"))));
+    }
+
+    public void btnStockManagementOnAction(ActionEvent actionEvent) throws IOException {
+        bodyPane.getChildren().clear();
+        bodyPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/StockManagementForm.fxml"))));
+    }
+
+    public void btnReportsManagementOnAction(ActionEvent actionEvent) throws IOException {
+        bodyPane.getChildren().clear();
+        bodyPane.getChildren().add(FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/ReportManagementForm.fxml"))));
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    public void btnEditProfileOnAction(ActionEvent actionEvent) {
+
     }
 }
